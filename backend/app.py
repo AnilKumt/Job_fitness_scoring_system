858035952e5ba12da7c1fbb13bc392e88e83e46b
AnilKumt <anilkumt51@gmail.com>
2025-09-29 09:34:22 +0530

    made backend flask application with dummy routes

---
+++ backend/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return f'User {escape(username)}'
