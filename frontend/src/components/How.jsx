633a0bf805fd8506ff58790107cac1dcdfdb8a55
AnilKumt <anilkumt51@gmail.com>
2025-09-30 07:03:13 +0530

    made all the components and page transitions

---
+++ frontend/src/components/How.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import transition from '../transition'
+
+const How = () => {
+  return (
+    <div className='bg-red-500 w-full h-screen'>How</div>
+  )
+}
+
+export default transition(How)
---
+++ frontend/src/components/Output.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import transition from '../transition'
+
+const Output = () => {
+  return (
+    <div className='bg-red-950 w-full h-screen'> Output</div>
+  )
+}
+
+export default transition(Output)
